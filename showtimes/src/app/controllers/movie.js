9a721c0054217613d4abb15b215bfb493c35e194
nattamon <joppy.pgh@gmail.com>
2022-06-23 21:22:17 +0700

    Fix error handling in controllers

--- showtimes/src/app/controllers/movie.js
+++ showtimes/src/app/controllers/movie.js
@@ -13,20 +13,22 @@ const createMovie = async (req, res) => {
 }
 
 const getMovieList = async (req, res) => {
-  const result = await movieModel.find({})
-  if (!result) {
+  try {
+    const result = await movieModel.find({})
+    res.status(200).json(result)
+  } catch (error) {
     throw NOT_FOUND_DATA
   }
-  res.status(200).json(result)
 }
 
 const getMovieByID = async (req, res) => {
   const { ID } = req.params
-  const result = await movieModel.findOne({ _id: ID })
-  if (!result) {
+  try {
+    const result = await movieModel.findOne({ _id: ID })
+    res.status(200).json(result)
+  } catch (error) {
     throw NOT_FOUND_DATA
   }
-  res.status(200).json(result)
 }
 
 export default {
--- users/src/app/controllers/user.js
+++ users/src/app/controllers/user.js
@@ -13,29 +13,32 @@ const createUser = async (req, res) => {
 }
 
 const getUserList = async (req, res) => {
-  const result = await userModel.find({})
-  if (!result) {
+  try {
+    const result = await userModel.find({})
+    res.status(200).json(result)
+  } catch (error) {
     throw NOT_FOUND_DATA
   }
-  res.status(200).json(result)
 }
 
 const getUserByEmail = async (req, res) => {
   const { email } = req.params
-  const result = await userModel.find({ email })
-  if (!result) {
+  try {
+    const result = await userModel.find({ email })
+    res.status(200).json(result)
+  } catch (error) {
     throw NOT_FOUND_DATA
   }
-  res.status(200).json(result)
 }
 
 const getUserByID = async (req, res) => {
   const { ID } = req.params
-  const result = await userModel.findOne({ _id: ID })
-  if (!result) {
+  try {
+    const result = await userModel.findOne({ _id: ID })
+    res.status(200).json(result)
+  } catch (error) {
     throw NOT_FOUND_DATA
   }
-  res.status(200).json(result)
 }
 
 export default {
